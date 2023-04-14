9ec56c557d76da721f40e6cc406f83a34a2f1d0d
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-04-14 11:50:25 -0500

    bootstrap icons added

--- clientsproject/src/Agenda/Agenda.jsx
+++ clientsproject/src/Agenda/Agenda.jsx
@@ -1,7 +1,7 @@
 export const Agenda = () => {
   return (
     <>
-      <form action="#">
+      <form action="#" className="my-5">
         <div className="row">
           <div className="col-12">
             <div class="input-group mb-3">
